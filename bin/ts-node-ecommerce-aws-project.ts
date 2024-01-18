5c5ec875b6ea5f9de5ce97bad56022be1e9ce9ee
silva4dev <web4dev.lucas@outlook.com>
2024-01-18 15:46:40 -0300

    feat: add integration lambda with api gateway

--- bin/ts-node-ecommerce-aws-project.ts
+++ bin/ts-node-ecommerce-aws-project.ts
@@ -1,6 +1,29 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { TsNodeEcommerceAwsProjectStack } from '../lib/ts-node-ecommerce-aws-project-stack';
+import { ProductsAppStack } from '../lib/productsApp-stack';
+import { ECommerceApiStack } from '../lib/ecommerceApi-stack';
 
 const app = new cdk.App();
-new TsNodeEcommerceAwsProjectStack(app, 'TsNodeEcommerceAwsProjectStack', {});
+
+const env: cdk.Environment = {
+  account: "470654920551",
+  region: "us-east-1" 
+}
+
+const tags = {
+  cost: 'ECommerce',
+  team: 'Lucas Alves'
+}
+
+const productsAppStack = new ProductsAppStack(app, 'ProductsApp', {
+  tags,
+  env
+})
+
+const eCommerceApiStack = new ECommerceApiStack(app, 'ECommerceApi', {
+  tags,
+  env,
+  productsFetchFunction: productsAppStack.productsFetchHandler
+})
+
+eCommerceApiStack.addDependency(productsAppStack)
